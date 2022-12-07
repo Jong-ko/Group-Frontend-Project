b6c0e0433c7ec0bed0e415e393921a718ac6a7cf
Jeremy Ongko <ongkojeremy@gmail.com>
2022-12-06 19:09:17 -0500

    fully completed city suggest feature

--- script.js
+++ script.js
@@ -9,16 +9,16 @@ document.addEventListener("click", function (event) {
 //Event listener for form submission
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-//   fetchWeather();
+  //   fetchWeather();
   citysuggest();
 });
 
 //Function that gets weather data from api and displays weather information based on city submitted in form
-async function fetchWeather(x) {
-//   const citySearch = document.getElementsByClassName("search")[0].value;
+async function fetchWeather(city) {
+  //   const citySearch = document.getElementsByClassName("search")[0].value;
   try {
     const results = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=c7dbf73d45d5d81e77ec919553791f1f&units=imperial`
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c7dbf73d45d5d81e77ec919553791f1f&units=imperial`
     );
     const weatherData = await results.json();
     console.log(weatherData);
@@ -88,20 +88,20 @@ async function citysuggest() {
       `http://api.openweathermap.org/geo/1.0/direct?q=${citySearch}&limit=5&appid=66f86253ffe193138c9b2af78822fd3e`
     );
     const citySearchJson = await results.json();
-    // console.log(citySearchJson);
     // If function if more then one city is found
     if (citySearchJson.length > 1) {
       // Loop to insert city suggestions into html
       for (let i = 0; i < citySearchJson.length; i++) {
         let suggestionsList = document.getElementById("citySuggestionsBox");
-        let cityoption = `${citySearch}, ${citySearchJson[i].state}, ${citySearchJson[i].country}`
+        let cityoption = `${citySearch}, ${citySearchJson[i].state}, ${citySearchJson[i].country}`;
         suggestionsList.innerHTML += `
-                      <a href="javascript:fetchWeather(${citySearch})">${cityoption}</a><br>`;
+            <a href="javascript:fetchWeather('${cityoption}')">${cityoption}</a><br>`;
+        // console.log(cityoption)
       }
     }
     // Else statement to directly input city into weather call if only 1 city is found
     else {
-        fetchWeather(citySearch)
+      fetchWeather(citySearch);
     }
   } catch (error) {
     console.log("Something went wrong fetching the Geocode API", error);
